493d2a34d10f8c0cc7a0f57887c11b47d53db7b0
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-20 15:58:14 -0600

    First version of the practice code

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _1_07.LINQed_List.Using_Custom_Types
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+        static Customer()
+        {
+            //
+        }
+    }
+}
