70dea1077efe3b9d4b9f85e9851c48000d17dff5
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-20 14:08:33 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_07.LINQed_List.Using_Custom_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSS.C-Sharp.1-07.LINQed_List.Using_Custom_Types
